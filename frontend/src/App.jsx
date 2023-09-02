62dc20a41671a85ae06c4e038de4d2f79b5c9b74
Deificz <99456747+Deificz@users.noreply.github.com>
2023-09-02 13:27:31 +0800

    Initialized frontend project files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-blue-400'>test</h1>
+    </>
+  )
+}
+
+export default App
