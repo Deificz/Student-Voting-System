e86543ddbd59e997dcffec3717a811c61eac1347
msllagas <94226891+msllagas@users.noreply.github.com>
2023-09-10 17:06:18 +0800

    test getting data from user's data

--- frontend/src/pages/Header View/Home.jsx
+++ frontend/src/pages/Header View/Home.jsx
@@ -1,14 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import Header from '../../components/Header.jsx'
 import SideBar from '../../components/display comps/sidebar.jsx'
 import Main from '../../components/display comps/main_home.jsx'
+import { UserDataContext } from '../../utils/Contexts.jsx';
+import { useAuth } from '../../utils/Auth.jsx';
 
 
 
 export default function StudentHome() {
+  const {userData} = useAuth();
+  console.log(userData)
+
  
   return (
     <>
+    <h1>{userData.email}</h1>
       <Header showNavbar={true}/>
       <div className='flex h-[100dvh]'>
         <SideBar/>
