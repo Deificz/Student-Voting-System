79ac3d1b9efeeff409f881651d153607d5c09ced
msllagas <94226891+msllagas@users.noreply.github.com>
2023-09-15 19:33:40 +0800

    vote entity and repository

---
+++ backend/src/main/java/com/dev/backend/entity/Vote.java
@@ -0,0 +1,51 @@
+package com.dev.backend.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Vote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "candidate_id")
+    private Candidate candidate;
+
+
+    public Vote() {
+    }
+
+    public Vote( User user, Candidate candidate) {
+        this.user = user;
+        this.candidate = candidate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Candidate getCandidate() {
+        return candidate;
+    }
+
+    public void setCandidate(Candidate candidate) {
+        this.candidate = candidate;
+    }
+}
---
+++ backend/src/main/java/com/dev/backend/repository/VoteRepository.java
@@ -0,0 +1,9 @@
+package com.dev.backend.repository;
+
+import com.dev.backend.entity.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+}
