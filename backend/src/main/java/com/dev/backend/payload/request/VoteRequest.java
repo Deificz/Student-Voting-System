c1abe221194c7cffc609d3a3301e397551a30a54
msllagas <mndyllagas@gmail.com>
2023-09-18 11:03:16 +0800

    change vote request structure, explicit handle for user id when voting

--- backend/src/main/java/com/dev/backend/payload/request/VoteRequest.java
+++ backend/src/main/java/com/dev/backend/payload/request/VoteRequest.java
@@ -1,18 +1,31 @@
 package com.dev.backend.payload.request;
 
+import java.util.List;
+
 public class VoteRequest {
-    private Long candidateId;
+    private Long userId;
+    private List<Long> candidateId;
     public VoteRequest() {
     }
 
-    public VoteRequest( Long candidateId, Long roleId) {
+    public VoteRequest(Long userId, List<Long> candidateId) {
+        this.userId = userId;
         this.candidateId = candidateId;
     }
-    public Long getCandidateId() {
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public List<Long> getCandidateId() {
         return candidateId;
     }
 
-    public void setCandidateId(Long candidateId) {
+    public void setCandidateId(List<Long> candidateId) {
         this.candidateId = candidateId;
     }
 }
