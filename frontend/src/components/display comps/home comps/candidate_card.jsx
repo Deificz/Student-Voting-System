86b1993f89c4da2deb4548e37635b6cd697d6ce1
Deificz <99456747+Deificz@users.noreply.github.com>
2023-09-25 09:31:51 +0800

    Updated candidate list UI

--- frontend/src/components/display comps/home comps/candidate_card.jsx
+++ frontend/src/components/display comps/home comps/candidate_card.jsx
@@ -2,15 +2,17 @@ import React, { useEffect, useState } from "react";
 import { useCandidates } from "../../../utils/Candidates";
 import { Link } from "react-router-dom";
 export default function candidate_card({ position }) {
-  const {getCandidates, status } = useCandidates();
+  const { getCandidates, status } = useCandidates();
   const [isAdmin, setIsAdmin] = useState(false);
-  const [candidates, setCandidates] = useState(JSON.parse(localStorage.getItem("Candidates")));
+  const [candidates, setCandidates] = useState(
+    JSON.parse(localStorage.getItem("Candidates"))
+  );
   useEffect(() => {
     getCandidates();
     const userData = JSON.parse(localStorage.getItem("userData"));
     const [role] = userData.roles;
     const boolAdmin = role === "ROLE_ADMIN";
-   
+
     setIsAdmin(boolAdmin);
   }, []);
 
@@ -31,7 +33,7 @@ export default function candidate_card({ position }) {
 
       <div
         id="candidate-panel"
-        className="m-5 md:grid md:grid-cols-3 md:overflow-y-auto"
+        className="flex flex-wrap md:overflow-y-auto justify-evenly"
       >
         {status === "Done" &&
           candidates
@@ -39,20 +41,20 @@ export default function candidate_card({ position }) {
             .map((candidate) => (
               <div
                 key={candidate.id}
-                className="flex flex-col justify-evenly h-[300px] w-[230px] md:w-[300px] md:h-[300px] 2xl:w-[400px] 2xl:h-[400px] border-2 border-color-blue m-10 rounded-xl shadow-card hover:-translate-y-3 transition-all duration-300"
+                className="flex flex-col justify-between h-[300px] w-[230px] md:w-[300px] md:h-[300px] 2xl:w-[400px] 2xl:h-[400px] border-2 my-10 border-color-blue rounded-xl shadow-card hover:-translate-y-3 transition-all duration-300 px-2 py-4"
               >
                 {isAdmin && (
                   <button className="self-end w-10 h-10 mr-3 text-gray-200 transition-all duration-300 border-4 border-white rounded-full bg-color-red hover:text-black hover:bg-white hover:border-color-red">
                     <i className="fa-solid fa-minus"></i>
                   </button>
                 )}
-                <div className="flex items-center px-2">
+                <div className="flex items-center">
                   <div className="h-[80px] w-[80px] bg-black rounded-full text-white flex justify-center items-center">
                     Logo
                   </div>
                   <h1 className="ml-5 font-semibold text-md md:text-xl 2xl:text-3xl">
                     {candidate.name}{" "}
-                    <span className="">
+                    <span className="text-xs md:text-md 2xl:text-xl">
                       <br />
                       {candidate.partylist}
                     </span>
--- frontend/src/components/display comps/vote comps/vote_done.jsx
+++ frontend/src/components/display comps/vote comps/vote_done.jsx
@@ -19,7 +19,7 @@ export default function vote_done({ userVotes, userName }) {
 
   return (
     <div className="flex items-center justify-center">
-      <div className="flex flex-col items-center p-5 mt-20 border-4 rounded-xl shadow-card md:px-32 md:py-10 2xl:px-44 border-color-blue">
+      <div className="flex flex-col items-center p-5 mt-20 md:mt-16 2xl:mt-24 border-4 rounded-xl shadow-card md:px-32 md:py-10 2xl:px-44 border-color-blue">
         <h1 className="mb-10 text-2xl font-bold md:text-2xl 2xl:text-4xl ">
           Hi{" "}
           <span className="text-color-blue">
