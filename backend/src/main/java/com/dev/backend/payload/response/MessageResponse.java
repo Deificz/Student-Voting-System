96054a608eabd9937c55bc550677b3023bfec137
msllagas <94226891+msllagas@users.noreply.github.com>
2023-09-30 10:51:53 +0800

    added delete mapping to delete candidate

--- backend/src/main/java/com/dev/backend/payload/response/MessageResponse.java
+++ backend/src/main/java/com/dev/backend/payload/response/MessageResponse.java
@@ -1,11 +1,23 @@
 package com.dev.backend.payload.response;
 
+import org.springframework.http.HttpStatus;
+
 public class MessageResponse {
+    private int status;
 
     private String message;
 
-    public MessageResponse(String message) {
+    public MessageResponse(int status, String message) {
+        this.status = status;
         this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
     }
 
     public String getMessage() {
